e417bfcc7f0f079d6fd2897a7c6c1c14b7d47d23
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-03-31 18:53:01 +0700

    untuk mendapatkan urutan angka yang berbeda

--- looping.cpp
+++ looping.cpp
@@ -11,4 +11,7 @@ int main(){
     for(int i=0;i<=4;i++){
             cout<<"Teknologi Informasi UMY"<<endl;
     }
+    cout<<endl;
+    //untuk mendapatkan urutan angka yang berbeda
+    srand(time(0));
 }
